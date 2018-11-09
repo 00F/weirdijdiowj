0a21dfd3990dff2b9a62d9af3ee0598d9649543a
` 0ff <39999036+00F@users.noreply.github.com>
2018-11-10 01:05:06 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,12 +1,30 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
+const developers = ['457763227745648640'];
+
 
 client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+  client.user.setActivity(".",{type: 'LISTENING'})
+  client.user.setStatus("dnd")
+});
+
+client.on('message', message => {
+  var argresult = message.content.split(` `).slice(1).join(' ');
+    if (!developers.includes(message.author.id)) return;
+
+if (message.content.startsWith('.wt')) {
+client.user.setActivity(argresult, {type:'WATCHING'});
+    message.channel.send(`${argresult} \` `)
+} else 
 if (message.content.startsWith('.st')) {
+client.user.setActivity(argresult , {type:'LISTENING'});
+    message.channel.send(` ${argresult} \` `)
+} else 
+if (message.content.startsWith('.ls')) {
   client.user.setGame(argresult, "https://www.twitch.tv/MeeRcY");
    message.channel.send(`${argresult} \` `)
-   
-     }
-          
+}
 });
+
 client.login(process.env.BOT_TOKEN)
